588c121343d7711d8a9b4747553a3d038fd79bb5
Blackbolt56 <144447632+Blackbolt56@users.noreply.github.com>
2024-02-08 00:42:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -55,9 +55,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = 'DENVER-V2'
 global.premium = 'true'
-global.packname = 'XLICON-V2' 
+global.packname = 'DENVER-V2' 
 global.author = 'Made by Denver' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
